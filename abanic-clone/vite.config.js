2690b17138aa4afda74a111b5b22868ebbddc9f5
Jorge <attiejorge89@gmail.com>
2025-07-19 18:23:01 -0300

    refactor: simplify vite.config.js and update vercel.json structure

--- abanic-clone/vite.config.js
+++ abanic-clone/vite.config.js
@@ -1,23 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-import path from 'path'; // Importe o módulo path
+const { defineConfig } = require('vite')
+const react = require('@vitejs/plugin-react')
 
-export default defineConfig({
+module.exports = defineConfig({
   plugins: [react()],
   css: {
     postcss: {
       plugins: [
-        tailwindcss(),
-        autoprefixer()
-      ],
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // Configuração correta do alias
-      '@/lib/utils': path.resolve(__dirname, './src/lib/utils.js') // Caminho absoluto
+        require('tailwindcss'),
+        require('autoprefixer')
+      ]
     }
   }
-});
+})
