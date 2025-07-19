de937377113fd6c29b56374d915fcef20306ce61
Jorge <attiejorge89@gmail.com>
2025-07-19 17:07:18 -0300

    refactor: update Vite configuration by removing unused plugins and options

--- abanic-clone/vite.config.js
+++ abanic-clone/vite.config.js
@@ -1,34 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
 
 export default defineConfig({
   plugins: [
     react({
-      jsxRuntime: 'classic' // Adicione esta linha para evitar conflitos
-    }),
-    tailwindcss()
+      jsxRuntime: 'automatic' // Isso resolve a necessidade de importar React explicitamente
+    })
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
   build: {
-    outDir: 'dist',
     rollupOptions: {
-      external: [], // Explicitamente defina módulos externos se necessário
+      external: [],
       output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
+        globals: {
+          'react': 'React',
+          'react-dom': 'ReactDOM'
         }
       }
     }
-  },
-  optimizeDeps: {
-    exclude: [], // Adicione módulos problemáticos aqui se necessário
-  },
+  }
 });
