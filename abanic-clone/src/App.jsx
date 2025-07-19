134745cff8b8e6617c90effbc242517b69993c66
Jorge <attiejorge89@gmail.com>
2025-07-19 18:04:25 -0300

    refactor: remove commented-out code and clean up App component

--- abanic-clone/src/App.jsx
+++ abanic-clone/src/App.jsx
@@ -1,29 +1,3 @@
-// import React from "react";
-// import "./App.css";
-// import Header from "./components/Header";
-// import HeroSection from "./components/HeroSection";
-// import CultureSection from "./components/CultureSection";
-// import NewsletterSection from "./components/NewsletterSection";
-// import Footer from "./components/Footer";
-// import { LanguageProvider } from "./context/LanguageContext";
-// function App() {
-//   return (
-//     <LanguageProvider>
-//       <div className="App">
-//         <Header />
-//         <main>
-//           <HeroSection />
-//           <CultureSection />
-//           <NewsletterSection />
-//         </main>
-//         <Footer />
-//       </div>
-//     </LanguageProvider>
-//   );
-// }
-
-// export default App;
-
 import React, { Suspense, lazy } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
